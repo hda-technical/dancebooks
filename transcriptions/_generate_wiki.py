398a4f686f72f73d5de634f68cb86610b860a9f9
Yuriy Chernyshov <thegeorg@yandex-team.ru>
2013-11-18 23:27:50 +0400

    Updated wiki generation script, updated wiki

--- transcriptions/_generate_wiki.py
+++ transcriptions/_generate_wiki.py
@@ -7,7 +7,7 @@ WIKI_PAGE = "wiki/Transcriptions.md"
 URL_PREFIX = "https://github.com/georgthegreat/dancebooks-bibtex/blob/master/transcriptions/"
 
 with open(WIKI_PAGE, "w") as wiki_file:
-	for tr in sys.argv[1:]:
+	for tr in sorted(sys.argv[1:]):
 		pretty_name, ext =  os.path.splitext(os.path.basename(tr.replace("_", " ")))
 		item_string = "* [{link}]({prefix}{name})\n".format(
 			link = pretty_name,
