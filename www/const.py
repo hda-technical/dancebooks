80c400e2671a011c0a892e62193dd6f6cd0b122b
Yuriy Chernyshov <thegeorg@yandex-team.ru>
2015-01-12 22:52:31 +0300

    Fixed validation errors

--- www/const.py
+++ www/const.py
@@ -80,7 +80,7 @@ LONG_LANG_MAP = {
 }
 
 #a structired pattern for file basename
-METADATA_PATTERN = r"(?:incomplete|[\w\s\-]+ copy)"
+METADATA_PATTERN = r"(?:incomplete|[\w\s\-']+ copy)"
 FILENAME_PATTERN = (
 	#year: digits can be replaced by dashes
 	r"\[(?P<year>[\d\-]+), "
