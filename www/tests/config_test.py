a1b954b9cd2696707b0cf4284a18b28a7b66ac55
Yuriy Chernyshov <thegeorg@yandex-team.ru>
2015-07-03 16:58:53 +0300

    Updated config test

--- www/tests/config_test.py
+++ www/tests/config_test.py
@@ -7,6 +7,8 @@ import config
 
 CONFIG_PATH_PRODUCTION = "../configs/dancebooks.production.conf"
 CONFIG_PATH_TESTING = "../configs/dancebooks.testing.conf"
+CONFIG_PATH_UNITTEST = "../configs/dancebooks.unittest.conf"
+CONFIG_PATH_DEVELOPMENT = "../configs/dancebooks.development.conf"
 
 class ConfigTest(unittest.TestCase):
 	def setUp(self):
@@ -17,10 +19,18 @@ class ConfigTest(unittest.TestCase):
 
 	def test_testing(self):
 		self.testing = config.Config(CONFIG_PATH_TESTING)
+		
+	def test_unittest(self):
+		self.unittest = config.Config(CONFIG_PATH_UNITTEST)
+		
+	def test_development(self):
+		self.development = config.Config(CONFIG_PATH_DEVELOPMENT)
 
 	def test_common_fields(self):
 		self.testing = config.Config(CONFIG_PATH_TESTING)
 		self.production = config.Config(CONFIG_PATH_PRODUCTION)
+		self.unittest = config.Config(CONFIG_PATH_UNITTEST)
+		self.development = config.Config(CONFIG_PATH_DEVELOPMENT)
 
 		test_redirections = self.testing.www.id_redirections.keys()
 		prod_redirections = self.production.www.id_redirections.keys()
