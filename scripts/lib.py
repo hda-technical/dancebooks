c3f14711fe7f2adce30d563cd89d3d5212589844
Yuriy Chernyshov <thegeorg@yandex-team.ru>
2019-04-03 23:15:28 +0300

    Implement BNF Candide downloader in lib.py

--- scripts/lib.py
+++ scripts/lib.py
@@ -412,7 +412,7 @@ def guess_tiles_number_y(url_maker):
 ###################
 
 @opster.command()
-def gallica(
+def bnfGallica(
 	id=("", "", "Id of the book to be downloaded (e. g. 'btv1b7200356s')")
 ):
 	"""
@@ -421,6 +421,56 @@ def gallica(
 	manifest_url = f"https://gallica.bnf.fr/iiif/ark:/12148/{id}/manifest.json"
 	output_folder = make_output_folder("gallica", id)
 	download_book_from_iiif_fast(manifest_url, output_folder)
+
+
+@opster.command()
+def bnfCandide(
+	id=("", "", "Id of the image to be downloaded (e. g. 'can_097')")
+):
+	"""
+	Downloads book from http://classes.bnf.fr/candide/
+	"""
+	#The site does not use any metadata and simply sends unnecessary requests to backend
+	#Using head requests to get maximum available zoom and
+	class UrlMaker(object):
+		def __init__(self, zoom):
+			self.zoom = zoom
+
+		def __call__(self, tile_x, tile_y):
+			probable_url = f"http://classes.bnf.fr/candide/images/5ci_nq/{id}/TileGroup0/{self.zoom}-{tile_x}-{tile_y}.jpg"
+			if requests.head(probable_url).status_code == 200:
+				return probable_url
+			else:
+				return None
+
+	MAX_ZOOM = 10
+	TILE_SIZE = 256
+	max_zoom = None
+	for test_zoom in range(MAX_ZOOM + 1):
+		if UrlMaker(test_zoom)(0, 0) is not None:
+			max_zoom = test_zoom
+		else:
+			#current zoom is not available - consider previous one to be maximal
+			break
+
+	assert(max_zoom is not None)
+	print(f"Guessed max_zoom={max_zoom}")
+	#The site does not use any metadata and simply sends unnecessary requests to backend
+	#Guessing tiles_number_x, tiles_number_y using HEAD requests with guessed max_zoom
+	#
+	#UrlMaker returns None when corresponding tile does not exist
+	#
+	#FIXME: one can save some requests using bisection here,
+	#but python standard library is too poor to have one
+	url_maker = UrlMaker(max_zoom)
+	tiles_number_x = guess_tiles_number_x(url_maker)
+	print(f"Guessed tiles_number_x={tiles_number_x}")
+	tiles_number_y = guess_tiles_number_y(url_maker)
+	print(f"Guessed tiles_number_y={tiles_number_y}")
+
+	policy = TileSewingPolicy(tiles_number_x, tiles_number_y, TILE_SIZE)
+	output_filename = make_output_filename(id.replace("/", "."))
+	download_and_sew_tiles(output_filename, url_maker, policy)	
 
 
 @opster.command()
