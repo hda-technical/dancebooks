6d1908f24a703040be9720106a5dfab685cd7281
Yuriy Chernyshov <thegeorg@yandex-team.ru>
2020-03-22 12:29:02 +0300

    Some improvements in rename_folios.py

--- scripts/rename_folios.py
+++ scripts/rename_folios.py
@@ -9,17 +9,24 @@ def main(
 	start=("", 1, "First folio number"),
 	prefix=("", "", "Prefix to be added to all folio numbers")
 ):
-	files = os.listdir(".")
+	filenames = []
+	for entry in os.scandir("."):
+		if not entry.is_file():
+			# take only files into account
+			continue
+		if entry.name.endswith(".py"):
+			# skip some well-known extensions
+			# generally, this filter must be improved
+			continue
+		filenames.append(entry.name)
+	
 	folio = int(start)
 	suffix = "r"
-	prefix = str(prefix)
-	for file in files:
-		if file.endswith(".py"):
-			continue
-		basename, ext = os.path.splitext(file)
+	for filename in sorted(filenames):
+		basename, ext = os.path.splitext(filename)
 		new_filename = f"{prefix}{folio:04d}{suffix}{ext}"
-		print(f"Renaming {file} to {new_filename}")
-		os.rename(file, new_filename)
+		print(f"Renaming {filename} to {new_filename}")
+		os.rename(filename, new_filename)
 		if suffix == "r":
 			suffix = "v"
 		else:
