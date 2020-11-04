10b59a3c730d6ffa24fefb4b7bc04046e1b5b49e
Yuriy Chernyshov <thegeorg@yandex-team.ru>
2020-11-04 13:05:01 +0300

    Move onb downloader into separate file

--- tools/downloader/main.py
+++ tools/downloader/main.py
@@ -761,34 +761,8 @@ def onb(
 	"""
 	Downloads book from http://onb.ac.at/
 	"""
-	# First, normalizing id
-	id = id.replace('/', '_')
-	if id.startswith("ABO"):
-		flavour = "OnbViewer"
-	elif id.startswith("DTL"):
-		flavour = "RepViewer"
-	else:
-		raise RuntimeError(f"Can not determine flavour for {id}")
-
-	# Second, obtaining JSESSIONID cookie value
-	viewer_url = f"http://digital.onb.ac.at/{flavour}/viewer.faces?doc={id}"
-	viewer_response = requests.get(viewer_url)
-	cookies = viewer_response.cookies
-	metadata_url = f"http://digital.onb.ac.at/{flavour}/service/viewer/imageData?doc={id}&from=1&to=1000"
-	metadata = get_json(metadata_url, cookies=cookies)
-	output_folder = make_output_folder("onb", id)
-	image_data = metadata["imageData"]
-	print(f"Going to download {len(image_data)} images")
-	for image in image_data:
-		query_args = image["queryArgs"]
-		image_id = image["imageID"]
-		image_url = f"http://digital.onb.ac.at/{flavour}/image?{query_args}&s=1.0&q=100"
-		output_filename = make_output_filename(output_folder, image_id, extension=None)
-		if os.path.isfile(output_filename):
-			print(f"Skip downloading existing image {image_id}")
-			continue
-		print(f"Downloading {image_id}")
-		get_binary(output_filename, image_url, cookies=cookies)
+	import onb
+	onb.get(id)
 
 
 @opster.command()
