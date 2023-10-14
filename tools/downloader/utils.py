fb8b03ffcf7ce4dde7ade4e001a3d81703978130
Yuriy Chernyshov <thegeorg@yandex-team.com>
2023-10-14 13:12:14 +0200

    Drop ImageMagick code completely

--- tools/downloader/utils.py
+++ tools/downloader/utils.py
@@ -1,11 +1,10 @@
 import functools
 import json
 import os
+import io
 import math
 import shutil
-import subprocess
 import time
-import uuid
 from xml.etree import ElementTree
 
 import PIL.Image
@@ -13,8 +12,8 @@ import requests
 
 
 # FIXME:
-# 	If the website responds with 4xx errors, changing User-Agent might help.
-# 	In particularly, CloudFlare works well with curl.
+#	If the website responds with 4xx errors, changing User-Agent might help.
+#	In particularly, CloudFlare works well with curl.
 #
 # USER_AGENT = "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:62.0) Gecko/20100101 Firefox/62.0"
 USER_AGENT = "curl/7.68.0"
@@ -101,6 +100,12 @@ def get_binary(output_filename, url_or_request, *args, **kwargs):
 			total_size += len(chunk)
 			file.write(chunk)
 	return total_size
+	
+
+@retry(try_count=5, delay=30, delay_backoff=1.5)
+def get_image(url_or_request, *args, **kwargs):
+	response = make_request(url_or_request, *args, stream=True, **kwargs)
+	return PIL.Image.open(io.BytesIO(response.content))
 
 
 def cleanup_filename(bad_name):
@@ -128,10 +133,6 @@ def make_output_filename(base, page=None, extension="bmp"):
 	return result
 
 
-def make_temporary_folder():
-	return str(uuid.uuid4())
-
-
 class TileSewingPolicy:
 	def __init__(self, tiles_number_x, tiles_number_y, tile_size, image_width=None, image_height=None, overlap=None):
 		self.tiles_number_x = tiles_number_x
@@ -150,94 +151,22 @@ class TileSewingPolicy:
 		return TileSewingPolicy(tiles_number_x, tiles_number_y, tile_size, image_width=width, image_height=height)
 
 
-def sew_tiles_with_pillow(folder, output_file, policy):
-    result = PIL.Image.new("RGB", (policy.image_width, policy.image_height))
-    for tile_x in range(policy.tiles_number_x):
-        for tile_y in range(policy.tiles_number_y):
-            tile_image = PIL.Image.open(f"{folder}/{tile_y:08d}_{tile_x:08d}.jpg")
-            result.paste(tile_image, (tile_x * policy.tile_size, tile_y * policy.tile_size))
-    result.save(output_file, "BMP")
-
-
-def sew_tiles_with_montage(folder, output_file, policy):
-	"""
-	Invokes montage tool from ImageMagick package to sew tiles together
-	"""
-	def format_magick_geometry(policy):
-		geometry = ""
-		if policy.tile_size is not None:
-			geometry += f"{policy.tile_size}x{policy.tile_size}"
-		if policy.overlap is not None:
-			geometry += f"-{policy.overlap}-{policy.overlap}"
-		if geometry:
-			#WARN:
-			#  Do not allow enlarging tiles.
-			#  Certain libraries (i. e. Gallica) use variable tile size
-			geometry += '>'
-		return geometry
-
-	def format_magick_tile(policy):
-		return f"{policy.tiles_number_x}x{policy.tiles_number_y}"
-
-	# Sewing tiles
-	cmd_line = [
-		"montage",
-		f"{folder}/*",
-		"-mode", "Concatenate"
-	]
-	geometry = format_magick_geometry(policy)
-	if geometry:
-		cmd_line += ["-geometry", geometry]
-	cmd_line += [
-		"-tile", format_magick_tile(policy),
-		output_file
-	]
-	print(f"Sewing tiles with:\n    {' '.join(cmd_line)}")
-	print("    WARN: if this stage seems to be slow, consider rising the limits in /etc/ImageMagick-6/policy.xml.")
-	subprocess.check_call(cmd_line)
-
-	if policy.image_width and policy.image_height:
-		# Cropping extra boundaries (right and bottom) added during sewing
-		cmd_line = [
-			"convert",
-			output_file,
-			"-extent", f"{policy.image_width}x{policy.image_height}",
-			output_file
-		]
-		print(f"Cropping output image with:\n    {' '.join(cmd_line)}")
-		subprocess.check_call(cmd_line)
-	elif policy.trim:
-		# Trimming extra boundaries automatically
-		cmd_line = [
-			"convert",
-			output_file,
-			"-trim",
-			output_file
-		]
-		print(f"Trimming output image with:\n    {' '.join(cmd_line)}")
-		subprocess.check_call(cmd_line)
-
-
 def download_and_sew_tiles(output_filename, url_maker, policy):
-	if os.path.exists(output_filename):
-		print(f"Skip downloading existing file {output_filename}")
-	tmp_folder = make_temporary_folder()
-	os.mkdir(tmp_folder)
-	try:
-		print(f"Downloading {policy.tiles_number_x}x{policy.tiles_number_y} tiled image ({policy.image_width}x{policy.image_height}) to {output_filename}")
-		for tile_x in range(policy.tiles_number_x):
-			for tile_y in range(policy.tiles_number_y):
-				if policy.reverse_axis_y:
-					MAX_TILE_NUMBER_Y = 50
-					tile_file = os.path.join(tmp_folder, f"{MAX_TILE_NUMBER_Y - tile_y:08d}_{tile_x:08d}.jpg")
-				else:
-					tile_file = os.path.join(tmp_folder, f"{tile_y:08d}_{tile_x:08d}.jpg")
-				url = url_maker(tile_x, tile_y)
-				get_binary(tile_file, url)
-		sew_tiles_with_pillow(tmp_folder, output_filename, policy)
-	finally:
-		if "KEEP_TEMP" not in os.environ:
-			shutil.rmtree(tmp_folder)
+	if policy.trim:
+		raise NotImplementedError("TODO: support trim in this code")
+	if policy.overlap is not None:
+		raise NotImplementedError("TODO: support overlap in this code")
+	if policy.reverse_axis_y:
+		raise NotImplementedError("TODO: support reverse_axis_y in this code")
+
+	result = PIL.Image.new("RGB", (policy.image_width, policy.image_height))
+	print(f"Downloading {policy.tiles_number_x}x{policy.tiles_number_y} tiled image ({policy.image_width}x{policy.image_height}) to {output_filename}")
+	for tile_x in range(policy.tiles_number_x):
+		for tile_y in range(policy.tiles_number_y):
+			url = url_maker(tile_x, tile_y)
+			tile_image = get_image(url)
+			result.paste(tile_image, (tile_x * policy.tile_size, tile_y * policy.tile_size))
+	result.save(output_filename, "BMP")
 
 
 def first(iterable):
@@ -254,7 +183,7 @@ def first(iterable):
 #
 # FIXME:
 #	one can save some requests using bisection here,
-# 	but python standard library is too poor to have one.
+#	but python standard library is too poor to have one.
 
 def guess_tiles_zoom(url_maker_maker):
 	MAX_ZOOM = 10
