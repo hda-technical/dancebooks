ff55e851333c26988d467efe0edf74f6fb90824e
Yuriy Chernyshov <thegeorg@yandex-team.com>
2025-11-24 22:52:46 +0100

    Validate BNE permalinks against regexp

--- dancebooks/const.py
+++ dancebooks/const.py
@@ -305,4 +305,5 @@ URL_REGEXPS = {
 	"retronews.fr": re.compile(rf"https://www.retronews.fr/journal/[\w\-]+/[\w\d\-]+/\d+/{_GUID}"),
 	"data.onb.ac.at": re.compile(r"https://data.onb.ac.at/rep/[0-9A-F]{8,10}"),
 	"musenalm.de": re.compile(r"https://musenalm.de/almanach/\d+/"),
+	"bdh.bne.es": re.compile(r"https://bdh.bne.es/bnesearch/detalle/bdh\d{10}"),
 }
